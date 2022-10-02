7ebe60a1d6897056fd1dadbebc6f86028488e572
ArxLancifer <a_christoforidis@hotmail.com>
2022-10-02 14:24:49 +0300

    Tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from 'react'
+
+
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+    id:1,
+    text: 'Wake up',
+    day: 'Oct 10th at 10am',
+    reminder: true
+    },
+    {
+    id:2,
+    text: 'Make coffee',
+    day: 'Oct 10th at 10:30am',
+    reminder: true
+    },
+    {
+    id:3,
+    text: 'Start coding',
+    day: 'Oct 10th at 11:00am',
+    reminder: true
+    },
+    {
+        id:4,
+        text: 'Meeting',
+        day: 'Oct 12th at 11:00am',
+        reminder: true
+    },
+])
+
+// Delete Task
+const deleteTask =(id)=>{
+  console.log('delete', id)
+}
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
+const Button = ({color, text, onClick}) => {
+
+
+
   return (
-      <button style={{backgroundColor:color}} className="btn">{text}</button>
+      <button onClick={onClick} style={{backgroundColor:color}} className="btn">{text}</button>
   )
 }
 
@@ -13,6 +16,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick:PropTypes.func,
 }
 
 export default Button

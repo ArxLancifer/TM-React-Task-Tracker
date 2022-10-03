835e818324e09b8e2e30b6cadec9895cf6af9060
ArxLancifer <a_christoforidis@hotmail.com>
2022-10-03 04:43:28 +0300

    Add tasks comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from 'react'
 
 
@@ -29,19 +30,36 @@ function App() {
         id:4,
         text: 'Meeting',
         day: 'Oct 12th at 11:00am',
-        reminder: true
+        reminder: false
     },
 ])
 
+
+// Add Task
+
+const addTask = (task)=>{
+  const id = Math.floor(Math.random()*10000)+1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
 // Delete Task
 const deleteTask =(id)=>{
-  console.log('delete', id)
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle reminde
+
+const toggleReminder = (id)=>{
+  setTasks(tasks.map((task)=>task.id === id 
+  ? {...task, reminder: !task.reminder} : task))
 }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />: <p>'No tasks to show'</p>}
     </div>
   );
 }
